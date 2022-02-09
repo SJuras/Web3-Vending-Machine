13226dae830180baf6b47507e8282fbc4f2ac239
SJuras <jurasstpko@gmailcom>
2022-02-09 22:05:50 +0100

    v1.1.4 added a truffle project.

--- vending-machine/pages/vending-machine.js
+++ vending-machine/pages/vending-machine.js
@@ -1,10 +1,29 @@
 import Head from 'next/head';
+import Web3 from 'web3';
+import { useState } from 'react';
 
 import 'bulma/css/bulma.css';
 import styles from '../styles/VendingMachine.module.css';
 
 
-export default function VendingMachine(){
+const VendingMachine = () => {
+  const [error, setError] = useState("");
+  let web3;
+
+  const connectWalletHandler = async () => {
+    if(typeof window !== "undefined" && typeof window.ethereum !== "undefined"){
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+        web3 = new Web3(window.ethereum);
+      } catch(err) {
+        setError(err.message);
+      }
+    } else {
+      // metamask not installed
+      console.log("Please install the MetaMask");
+    }
+  }
+
   return(
     <div className={styles.main}>
       <Head>
@@ -13,12 +32,12 @@ export default function VendingMachine(){
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <nav className="navbar mt-4 mb-4">
-        <div className="container navbar-back">
+        <div className="container">
           <div className="navbar-brand">
             <h1>Brendan</h1>
           </div>
           <div className="navbar-end">
-            <button className="button is-primary">
+            <button onClick={connectWalletHandler} className="button is-primary">
               Connect Wallet
             </button>
           </div>
@@ -31,6 +50,14 @@ export default function VendingMachine(){
         </div>
       </section>
 
+      <section>
+        <div className="containerhas-text-danger">
+          <p>{error}</p>
+        </div>
+      </section>
+
     </div>
   );
 }
+
+export default VendingMachine;
