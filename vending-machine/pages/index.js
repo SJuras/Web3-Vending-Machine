b3263dee1baf6f151697008e7034d22b79edb8a3
SJuras <jurasstpko@gmailcom>
2022-02-10 21:41:53 +0100

    v1.2.5 intro text done.

--- vending-machine/pages/index.js
+++ vending-machine/pages/index.js
@@ -66,45 +66,62 @@ export default function Home() {
         </div>
       </div>
 
-      <h2 className="font-Infinite text-brendanPink">BRENDAN</h2>
-      <h1 className="font-Ubuntu">Decentralized Vending Machine</h1>
-      <p className="font-Ubuntu">
-        Hi there, I'm Brendan, decentralized vending machine powered by a smart contract.
-      </p>
-      <p className="font-Ubuntu">
-        My smart contract is deployed on Rinkeby testnet,<br />
-        that's the network you should connect your MetaMask to.
-      </p>
-      <p className="font-Ubuntu">
-        If you don't have MetaMask installed yet, no problem, you can get it here:
-      </p>
-      <a href="https://metamask.io/" className="font-Ubuntu text-brendanPink font-bold">metamask.io</a>
-      <p className="font-Ubuntu">
-        First, let's get you some test Ether. <br />
-        You can get that from one of Rinkeby testnet faucets:
-      </p>
-      <ul>
-        <li><a href="https://faucet.rinkeby.io/" className="font-Ubuntu text-brendanPink font-bold">faucet.rinkeby.io</a></li>
-        <li><a href="https://rinkeby.faucet.epirus.io/" className="font-Ubuntu text-brendanPink font-bold">rinkeby.faucet.epirus.io</a></li>
-        <li><a href="https://faucets.chain.link/rinkeby" className="font-Ubuntu text-brendanPink font-bold">faucets.chain.link/rinkeby</a></li>
-      </ul>
-      <p className="font-Ubuntu">
-        Now you are ready to buy some donuts. Unfortunately, donuts are not real, this is just a testing phase.<br />
-        However, in next version, the donuts you can buy will be NFTs so stay tuned for the latest version of this dApp.
-      </p>
-      <p className="font-Ubuntu">
-        A donut will costs 1 Gwei, plus the Gas fee.
-      </p>
-      <p className="font-Ubuntu">
-        Good, now let's go get you some donuts.
-      </p>
-      <Link href="/vending-machine">
-        <button className="font-Ubuntu bg-brendanPink p-2 text-brendanWhite">To Veding Machine</button>
-      </Link>
-      <p className="font-Ubuntu">
-        Brendan was inspired by a character from the game Cyberpunk 2077.
-      </p>
+      <div className="w-full p-4 bg-brendanPink">
 
+        <div className="w-full md:w-2/3 bg-white text-center mx-auto rounded-lg p-4 m-4">
+          <div className="py-6">
+            <Link href="/vending-machine">
+              <button className="font-Ubuntu text-xl bg-brendanPink px-6 py-4 text-brendanWhite rounded">Buy Donuts</button>
+            </Link>
+            <p>
+              First time here? <br />
+              Scroll down to learn all about how to use Vending Machine.
+            </p>
+          </div>
+
+          <p className="font-Ubuntu">
+            Hi there, I'm Brendan, decentralized vending machine powered by a smart contract.
+          </p>
+          <p className="font-Ubuntu">
+            My smart contract is deployed on Rinkeby testnet,<br />
+            that's the network you should connect your MetaMask to.
+          </p>
+          <p className="font-Ubuntu">
+            If you don't have MetaMask installed yet, no problem, you can get it here:
+          </p>
+          <a href="https://metamask.io/" className="font-Ubuntu text-brendanPink font-bold">metamask.io</a>
+          <p className="font-Ubuntu mt-4">
+            First, let's get you some test Ether. <br />
+            You can get that from one of Rinkeby testnet faucets:
+          </p>
+          <ul className="mb-4">
+            <li><a href="https://faucet.rinkeby.io/" className="font-Ubuntu text-brendanPink font-bold">faucet.rinkeby.io</a></li>
+            <li><a href="https://rinkeby.faucet.epirus.io/" className="font-Ubuntu text-brendanPink font-bold">rinkeby.faucet.epirus.io</a></li>
+            <li><a href="https://faucets.chain.link/rinkeby" className="font-Ubuntu text-brendanPink font-bold">faucets.chain.link/rinkeby</a></li>
+          </ul>
+          <p className="font-Ubuntu">
+            Now you are ready to buy some donuts. Unfortunately, donuts are not real, this is just a testing phase.<br />
+            However, in next version, the donuts you can buy will be NFTs so stay tuned for the latest version of this dApp.
+          </p>
+          <p className="font-Ubuntu">
+            A donut will costs 1 Gwei, plus the Gas fee.
+          </p>
+          <p className="font-Ubuntu mb-2">
+            Good, now let's go get you some donuts.
+          </p>
+          <Link href="/vending-machine">
+            <button className="font-Ubuntu bg-brendanPink p-2 text-brendanWhite rounded mb-4">To Veding Machine</button>
+          </Link>
+          <p className="font-Ubuntu">
+            Now, you might remember me from a certain game. That's right, Cyberpunk 2077.
+          </p>
+          <p>
+            You thought they have overwritten me? <br />
+            Don't worry, I escaped and found my new home on the blockchain where I can <br />
+            be free to communicate with humans, such as yourself and provide them with tasty snacks.
+          </p>
+        </div>
+      </div>
 
       <Footer />
     </div>
